c0b7150d1b61d2f2686582848da59bce0ee1a550
LanchuNaik <134943642+LanchuNaik@users.noreply.github.com>
2023-08-08 19:36:34 +0530

    Add files via upload
    Responsive SocialBook Clone

---
+++ script.js
@@ -0,0 +1,32 @@
+var settingMenu=document.querySelector('.settings-menu');
+var darkbtn=document.querySelector('.dark-btn');
+
+function settingMenuToggle(){
+settingMenu.classList.toggle("settings-menu-height");
+}
+
+darkbtn.onclick=function darkModeOn(){
+  darkbtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+
+  if(localStorage.getItem("theme")=="light"){
+    localStorage.setItem("theme","dark");
+  }
+  else{
+    localStorage.setItem("theme","light");
+  }
+
+}
+
+if(localStorage.getItem("theme")=="light"){
+  darkbtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme")=="dark"){
+darkbtn.classList.add("dark-btn-on");
+document.body.classList.add("dark-theme");
+}
+else{
+  localStorage.setItem("theme","light");
+}
+
